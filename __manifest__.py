12143d93f226f49fd19cb41871b1bb9b7111adaa
papi <1mundo.it@gmail.com>
2018-05-22 22:40:51 -0700

    codigo_inicial_de_trabajo

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+	'name': 'To-Do Application',
+	'description': 'Manage your personal To-Do Tasks.',
+	'author': 'Papi',
+	'depends': ['base'],
+	'application': True,
+	'data': ['views/todo_menu.xml'],
+}
