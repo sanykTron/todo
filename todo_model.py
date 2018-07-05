93d5ab801b4b28c15059e75e7bd0b4cac62baac0
papi <1mundo.it@gmail.com>
2018-07-04 23:14:56 -0700

    logica de botones

--- todo_model.py
+++ todo_model.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
+
 class TodoTask(models.Model):
 	_name = 'todo.task'
 	_description = 'To-do Task'
@@ -8,3 +9,16 @@ class TodoTask(models.Model):
 	name = fields.Char('Description', required=True)
 	is_done = fields.Boolean('Done?')
 	active = fields.Boolean('Active?', default=True)
+
+	@api.multi
+	def do_toggle_done(self):
+		for task in self:
+			task.is_done = not task.is_done
+
+		return True
+
+	@api.multi
+	def do_clear_done(self):
+		dones = self.search([('is_done','=',True)])
+		dones.write({'active':False})
+		return True
